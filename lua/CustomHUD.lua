8aea65e3cb72aba0e141a9b931a33a4441fb6ade
BangL <henno.rickowski@gmail.com>
2017-03-29 04:49:35 +0200

    simplify code to hide stamina-meter, down-counter and detection-meter when in custody, apply custom condition icon color in CustomHUD as well

--- lua/CustomHUD.lua
+++ lua/CustomHUD.lua
@@ -4,45 +4,6 @@
 print_info = print_info or function(...) WolfHUD:print_log(string.format(...), "info") end
 print_warning = print_warning or function(...) WolfHUD:print_log(string.format(...), "warning") end
 
-if RequiredScript == "lib/managers/hudmanagerpd2" then
-	local set_player_condition_original = HUDManager.set_player_condition
-	local set_mugshot_custody_original = HUDManager.set_mugshot_custody
-	local set_mugshot_normal_original = HUDManager.set_mugshot_normal
-
-	function HUDManager:_mugshot_id_to_panel_id(id)
-		for _, data in pairs(managers.criminals:characters()) do
-			if data.data.mugshot_id == id then
-				return data.data.panel_id
-			end
-		end
-	end
-
-	function HUDManager:set_player_condition(icon_data, text)
-		set_player_condition_original(self, icon_data, text)
-		if icon_data == "mugshot_in_custody" then
-			self._teammate_panels[self.PLAYER_PANEL]:set_player_in_custody(true)
-		elseif icon_data == "mugshot_normal" then
-			self._teammate_panels[self.PLAYER_PANEL]:set_player_in_custody(false)
-		end
-	end
-
-	function HUDManager:set_mugshot_custody(id, ...)
-		local panel_id = self:_mugshot_id_to_panel_id(id)
-		if panel_id then
-			self._teammate_panels[panel_id]:set_player_in_custody(true)
-		end
-		return set_mugshot_custody_original(self, id, ...)
-	end
-
-	function HUDManager:set_mugshot_normal(id, ...)
-		local panel_id = self:_mugshot_id_to_panel_id(id)
-		if panel_id then
-			self._teammate_panels[panel_id]:set_player_in_custody(false)
-		end
-		return set_mugshot_normal_original(self, id, ...)
-	end
-end
-
 if not WolfHUD:getSetting({"CustomHUD", "ENABLED"}, true) then
 	if RequiredScript == "lib/managers/hudmanagerpd2" then
 
@@ -63,11 +24,13 @@ if not WolfHUD:getSetting({"CustomHUD", "ENABLED"}, true) then
 
 		local init_original = HUDTeammate.init
 		local set_name_original = HUDTeammate.set_name
+		local set_condition_original = HUDTeammate.set_condition
 
 		function HUDTeammate:init(...)
 			init_original(self, ...)
 			self._setting_prefix = self._main_player and "PLAYER" or "TEAMMATE"
-			self._panel:child("condition_icon"):set_color(WolfHUD:getColorSetting({"CustomHUD", self._setting_prefix, "CONDITION_ICON_COLOR"}, Color.white))
+			self._condition_icon = self._panel:child("condition_icon")
+			self._condition_icon:set_color(WolfHUD:getColorSetting({"CustomHUD", self._setting_prefix, "CONDITION_ICON_COLOR"}, Color.white))
 			if self._main_player and not HUDManager.CUSTOM_TEAMMATE_PANELS then
 				self:_create_stamina_circle()
 			end
@@ -173,13 +136,15 @@ if not WolfHUD:getSetting({"CustomHUD", "ENABLED"}, true) then
 			end
 		end
 
-		function HUDTeammate:set_player_in_custody(value)
-			self:set_stamina_meter_visibility(not value and WolfHUD:getSetting({"CustomHUD", "PLAYER", "STAMINA"}, true))
+		function HUDTeammate:set_condition(icon_data, ...)
+			local visible = icon_data ~= "mugshot_normal"
+			self:set_stamina_meter_visibility(not visible and WolfHUD:getSetting({"CustomHUD", "PLAYER", "STAMINA"}, true))
 			if HUDManager.DOWNS_COUNTER_PLUGIN and self._downs_counter and self._detection_counter then
-				local disabled = value or not WolfHUD:getSetting({"CustomHUD", self._setting_prefix, "DOWNCOUNTER"}, true) or self._ai
+				local disabled = visible or not WolfHUD:getSetting({"CustomHUD", self._setting_prefix, "DOWNCOUNTER"}, true) or self._ai
 				self._downs_counter:set_visible(not disabled and not managers.groupai:state():whisper_mode() or self:down_amount() > 0)
 				self._detection_counter:set_visible(not disabled and not self._downs_counter:visible())
 			end
+			set_condition_original(self, icon_data, ...)
 		end
 
 	end
@@ -913,15 +878,6 @@ if RequiredScript == "lib/managers/hud/hudteammate" then
 		self:call_listeners("damage_taken")
 	end
 
-	function HUDTeammateCustom:set_player_in_custody(value)
-		self._player_status._stamina_radial:set_visible(not value and self._player_status._is_local_player and self._settings.STAMINA)
-		if HUDManager.DOWNS_COUNTER_PLUGIN and self._downs_counter and self._detection_counter then
-			local disabled = value or not self._settings.DOWNCOUNTER
-			self._player_status._downs_counter:set_visible(not disabled and (not managers.groupai:state():whisper_mode() or self._player_status:down_amount() > 0))
-			self._player_status._detection_counter:set_visible(not disabled and not self._player_status._downs_counter:visible())
-		end
-	end
-
 	--Failsafe for unhandled functions
 	for id, ptr in pairs(HUDTeammate) do
 		if type(ptr) == "function" then
@@ -1628,7 +1584,7 @@ if RequiredScript == "lib/managers/hud/hudteammate" then
 		self._condition_icon = self._panel:bitmap({
 			name = "condition_icon",
 			visible = false,
-			color = Color.white,
+			color = WolfHUD:getColorSetting({"CustomHUD", self._setting_prefix, "CONDITION_ICON_COLOR"}, Color.white),
 			h = size,
 			w = size,
 		})
@@ -1862,7 +1818,7 @@ if RequiredScript == "lib/managers/hud/hudteammate" then
 		self._condition_icon = self._panel:bitmap({
 			name = "condition_icon",
 			visible = false,
-			color = Color.white,
+			color = WolfHUD:getColorSetting({"CustomHUD", self._setting_prefix, "CONDITION_ICON_COLOR"}, Color.white),
 			h = self._size,
 			w = self._size,
 			layer = 10,
@@ -1978,7 +1934,7 @@ if RequiredScript == "lib/managers/hud/hudteammate" then
 	function PlayerInfoComponent.PlayerStatus:update_settings()
 		self._stamina_radial:set_visible(self._is_local_player and self._settings.STAMINA)
 
-		local disabled = not (HUDManager.DOWNS_COUNTER_PLUGIN and self._settings.DOWNCOUNTER)
+		local disabled = self._condition_icon:visible() or not (HUDManager.DOWNS_COUNTER_PLUGIN and self._settings.DOWNCOUNTER)
 		self._downs_counter:set_visible(not disabled and (not managers.groupai:state():whisper_mode() or self:down_amount() > 0))
 		self._detection_counter:set_visible(not disabled and not self._downs_counter:visible())
 
@@ -2042,7 +1998,7 @@ if RequiredScript == "lib/managers/hud/hudteammate" then
 			self._downs_counter:set_text(tostring(self._downs))
 			local progress = math.clamp(self:down_amount() / self._max_downs, 0, 1)
 			self._downs_counter:set_color(math.lerp(Color.white, Color(1, 1, 0.2, 0), progress))
-			local disabled = not (HUDManager.DOWNS_COUNTER_PLUGIN and self._settings.DOWNCOUNTER)
+			local disabled = self._condition_icon:visible() or not (HUDManager.DOWNS_COUNTER_PLUGIN and self._settings.DOWNCOUNTER)
 			self._downs_counter:set_visible(not disabled and (not managers.groupai:state():whisper_mode() or self:down_amount() > 0))
 			self._detection_counter:set_visible(not disabled and not self._downs_counter:visible())
 		end
@@ -2080,14 +2036,14 @@ if RequiredScript == "lib/managers/hud/hudteammate" then
 			self._detection_counter:set_text(utf8.char(57363) .. tostring(self._risk))
 			self._detection_counter:set_color(color)
 
-			local disabled = not (HUDManager.DOWNS_COUNTER_PLUGIN and self._settings.DOWNCOUNTER)
+			local disabled = self._condition_icon:visible() or not (HUDManager.DOWNS_COUNTER_PLUGIN and self._settings.DOWNCOUNTER)
 			self._downs_counter:set_visible(not disabled and not managers.groupai:state():whisper_mode() or self:down_amount() > 0)
 			self._detection_counter:set_visible(not disabled and not self._downs_counter:visible())
 		end
 	end
 
 	function PlayerInfoComponent.PlayerStatus:_whisper_mode_change(event, key, status)
-		local disabled = not (HUDManager.DOWNS_COUNTER_PLUGIN and self._settings.DOWNCOUNTER)
+		local disabled = self._condition_icon:visible() or not (HUDManager.DOWNS_COUNTER_PLUGIN and self._settings.DOWNCOUNTER)
 		self._downs_counter:set_visible(not disabled and (not status or self:down_amount() > 0))
 		self._detection_counter:set_visible(not disabled and not self._downs_counter:visible())
 	end
@@ -2130,6 +2086,13 @@ if RequiredScript == "lib/managers/hud/hudteammate" then
 		end
 
 		self._condition_icon:set_visible(visible)
+
+		self._stamina_radial:set_visible(not visible and self._is_local_player and self._settings.STAMINA)
+		if HUDManager.DOWNS_COUNTER_PLUGIN and self._downs_counter and self._detection_counter then
+			local disabled = visible or not self._settings.DOWNCOUNTER
+			self._downs_counter:set_visible(not disabled and (not managers.groupai:state():whisper_mode() or self:down_amount() > 0))
+			self._detection_counter:set_visible(not disabled and not self._downs_counter:visible())
+		end
 	end
 
 	function PlayerInfoComponent.PlayerStatus:start_timer(time)
