8a348174101cedf3fd8c8d7956c21c37a5b10b14
BangL <henno.rickowski@gmail.com>
2024-04-22 20:09:14 +0200

    name tweak fixups

--- modules/HUDTweaks.lua
+++ modules/HUDTweaks.lua
@@ -1,19 +1,40 @@
 if string.lower(RequiredScript) == "lib/managers/hud/hudteammatebase" then
-    function HUDTeammateBase:wh_refresh_level()
-        self._player_level:set_x(0)
-        self._player_level:set_y(1)
-        self._player_level:set_h(self._player_name:h())
-        self._player_level:set_halign(self._player_name:halign())
-        self._player_level:set_font_size(self._player_name:font_size())
-        self:wh_refresh_name()
+    function HUDTeammateBase:wh_refresh_level(is_player)
+        if alive(self._player_level) and alive(self._player_name) then
+            self._player_level:set_x(0)
+            self._player_level:set_y(is_player and 1 or 0)
+            self._player_level:set_h(self._player_name:h())
+            self._player_level:set_valign("left")
+            self._player_level:set_halign(self._player_name:halign())
+            self._player_level:set_font_size(self._player_name:font_size())
+            MenuNodeBaseGui.make_fine_text(self._player_level)
+            self:wh_refresh_name()
+        end
     end
 
-    function HUDTeammateBase:wh_refresh_name()
-        self._player_name:set_x(self._player_level:w() + 4)
-        local name_w = select(3, self._player_name:text_rect())
-        local chat_x = math.min(name_w, self._player_name:w())
-        self._voice_chat_panel:set_left(self._player_name:x() + chat_x)
-        self._voice_chat_panel:set_top(self._player_name:y())
+    function HUDTeammateBase:wh_refresh_name(is_player)
+        if alive(self._player_level) and alive(self._player_name) then
+            self._player_name:set_x(self._player_level:w() + 4)
+            self._player_name:set_y(0)
+            if alive(self._voice_chat_panel) then
+                local name_w = select(3, self._player_name:text_rect())
+                local chat_x = math.min(name_w, self._player_name:w())
+                self._voice_chat_panel:set_left(self._player_name:x() + chat_x)
+                self._voice_chat_panel:set_top(self._player_name:y())
+            end
+        end
+    end
+
+    function HUDTeammateBase:wh_set_unit(unit)
+        if alive(unit) and managers.criminals and tweak_data.chat_colors and alive(self._player_name) then
+            local color_id = managers.criminals:character_color_id_by_unit(unit)
+            if color_id then
+                local color = tweak_data.chat_colors[color_id]
+                if color then
+                    self._player_name:set_color(color)
+                end
+            end
+        end
     end
 elseif string.lower(RequiredScript) == "lib/managers/hud/hudteammatepeer" then
     local set_name_original = HUDTeammatePeer.set_name
@@ -21,22 +42,27 @@ elseif string.lower(RequiredScript) == "lib/managers/hud/hudteammatepeer" then
 
     function HUDTeammatePeer:set_name(name, ...)
         set_name_original(self, name, ...)
-        if self._player_name and managers.criminals
-            and WolfgangHUD:getSetting({ "HUD", "COLORIZE_NAMES" }, false) then
-            self._player_name:set_color(
-                tweak_data.chat_colors
-                [managers.criminals:character_color_id_by_name(managers.criminals:character_name_by_panel_id(self._id))]
-            )
-            if WolfgangHUD:getSetting({ "HUD", "LEVELS_BEFORE_NAME" }, false) then
-                self:wh_refresh_name()
+        if not (self._player_name and managers.criminals and tweak_data.chat_colors) then
+            return
+        end
+
+        if WolfgangHUD:getSetting({ "HUD", "COLORIZE_NAMES" }, false) then
+            local peer_id = self:peer_id()
+            if peer_id then
+                local unit = managers.criminals:character_unit_by_peer_id(peer_id)
+                self:wh_set_unit(unit)
             end
+        end
+
+        if WolfgangHUD:getSetting({ "HUD", "LEVELS_BEFORE_NAME" }, false) then
+            self:wh_refresh_name(false)
         end
     end
 
     function HUDTeammatePeer:set_level(level, ...)
         set_level_original(self, level, ...)
         if WolfgangHUD:getSetting({ "HUD", "LEVELS_BEFORE_NAME" }, false) then
-            self:wh_refresh_level()
+            self:wh_refresh_level(false)
         end
     end
 elseif string.lower(RequiredScript) == "lib/managers/hud/hudteammateplayer" then
@@ -45,22 +71,23 @@ elseif string.lower(RequiredScript) == "lib/managers/hud/hudteammateplayer" then
 
     function HUDTeammatePlayer:set_name(name, ...)
         set_name_original(self, name, ...)
-        if self._player_name and managers.criminals and managers.player
-            and WolfgangHUD:getSetting({ "HUD", "COLORIZE_NAMES" }, false) then
-            self._player_name:set_color(
-                tweak_data.chat_colors
-                [managers.criminals:character_color_id_by_unit(managers.player:player_unit())]
-            )
-            if WolfgangHUD:getSetting({ "HUD", "LEVELS_BEFORE_NAME" }, false) then
-                self:wh_refresh_name()
-            end
+        if not (self._player_name and managers.criminals and managers.player and managers.player.player_unit) then
+            return
+        end
+
+        if WolfgangHUD:getSetting({ "HUD", "COLORIZE_NAMES" }, false) then
+            self:wh_set_unit(managers.player:player_unit())
+        end
+
+        if WolfgangHUD:getSetting({ "HUD", "LEVELS_BEFORE_NAME" }, false) then
+            self:wh_refresh_name(true)
         end
     end
 
     function HUDTeammatePlayer:set_level(level, ...)
         set_level_original(self, level, ...)
         if WolfgangHUD:getSetting({ "HUD", "LEVELS_BEFORE_NAME" }, false) then
-            self:wh_refresh_level()
+            self:wh_refresh_level(true)
         end
     end
 end
