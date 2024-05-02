e64b9c8cf2a20c1e429d46e807e328d233b61fa9
BangL <henno.rickowski@gmail.com>
2024-05-02 18:13:11 +0200

    show more half-stealthable raids, add gold diggers to list

--- modules/StealthableMissions.lua
+++ modules/StealthableMissions.lua
@@ -4,6 +4,7 @@ if string.lower(RequiredScript) == "lib/managers/menu/raid_menu/controls/raidgui
     local icon_size = tweak_data.gui.font_sizes.small - 2
     --local icon_color = tweak_data.gui.colors.raid_dirty_white
     local partially_stealthable = {
+        ["fury_railway"] = true,  -- Gold Diggers               // ends when closing gate
         ["radio_defense"] = true, -- Wiretap                    // ends when Lange climbed the tower
         ["bunker_test"] = true,   -- Bunker Busters             // ends when blowing up the dynamite
         ["spies_test"] = true,    -- Extraction                 // ends when using the radio
@@ -17,9 +18,9 @@ if string.lower(RequiredScript) == "lib/managers/menu/raid_menu/controls/raidgui
         end
 
         local raid_data = tweak_data.operations.missions[data.value]
-        if raid_data.start_in_stealth and raid_data.stealth_bonus then
+        if raid_data.start_in_stealth then
             local x, _, w, _ = self._item_label:text_rect()
-            self._stealhtable_icon = self:_init_stealthable_icon(x + w + 4, 0, partially_stealthable[data.value])
+            self._stealhtable_icon = self:_init_stealthable_icon(x + w + 4, 0, raid_data.stealth_bonus == nil or partially_stealthable[data.value])
             self._stealhtable_icon:set_center_y(RaidGUIControlListItemRaids.NAME_CENTER_Y)
         end
     end
