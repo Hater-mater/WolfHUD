dc280ba970f807f8387db44d3d1a412fd352e41c
BangL <henno.rickowski@gmail.com>
2024-05-02 18:12:38 +0200

    another attempt to fix 'straight to main menu' once and for all!

--- modules/StraightToMainMenu.lua
+++ modules/StraightToMainMenu.lua
@@ -1,24 +1,19 @@
 if string.lower(RequiredScript) == "lib/setups/menusetup" then
-
     local init_game_original = MenuSetup.init_game
 
     function MenuSetup:init_game(...)
         local gsm = init_game_original(self, ...)
-        if WolfgangHUD:getSetting({"MENU", "STRAIGHT_TO_MAIN_MENU"}, false) then
+        if WolfgangHUD:getSetting({ "MENU", "STRAIGHT_TO_MAIN_MENU" }, false) then
             gsm:set_boot_intro_done(true)
             gsm:change_state_by_name("menu_titlescreen")
         end
         return gsm
     end
-
 elseif string.lower(RequiredScript) == "lib/states/menutitlescreenstate" then
-
     local get_start_pressed_controller_index_original = MenuTitlescreenState.get_start_pressed_controller_index
-    local _load_savegames_done_original = MenuTitlescreenState._load_savegames_done
 
     function MenuTitlescreenState:get_start_pressed_controller_index(...)
-        self.SILENCED = false
-        if WolfgangHUD:getSetting({"MENU", "STRAIGHT_TO_MAIN_MENU"}, false) then
+        if WolfgangHUD:getSetting({ "MENU", "STRAIGHT_TO_MAIN_MENU" }, false) then
             local num_connected = 0
             local keyboard_index = nil
             for index, controller in ipairs(self._controller_list) do
@@ -30,20 +25,9 @@ elseif string.lower(RequiredScript) == "lib/states/menutitlescreenstate" then
                 end
             end
             if num_connected == 1 and keyboard_index ~= nil then
-                self.SILENCED = true
                 return keyboard_index
             end
         end
         return get_start_pressed_controller_index_original(self, ...)
     end
-
-    function MenuTitlescreenState:_load_savegames_done(...)
-        if self.SILENCED then
-            self.SILENCED = false
-            self:gsm():change_state_by_name("menu_main")
-        else
-            _load_savegames_done_original(self, ...)
-        end
-    end
-
 end
