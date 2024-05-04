d74114738842f41779ea26ba2fb766fa142ae841
BangL <henno.rickowski@gmail.com>
2024-05-04 15:02:26 +0200

    fixed meta data for the new stealthable icons, reviewed by "Hater"

--- modules/StealthableMissions.lua
+++ modules/StealthableMissions.lua
@@ -2,14 +2,26 @@ if string.lower(RequiredScript) == "lib/managers/menu/raid_menu/controls/raidgui
     local _layout_raid_name_original = RaidGUIControlListItemRaids._layout_raid_name
 
     local icon_size = tweak_data.gui.font_sizes.small - 2
+
     --local icon_color = tweak_data.gui.colors.raid_dirty_white
-    local partially_stealthable = {
+    local override_partially_stealthable = {
+        ["gold_rush"] = true,     -- Gold Rush                  // Starts in stealth, but ends when shooting the hole into the wall/door :D
         ["fury_railway"] = true,  -- Gold Diggers               // ends when closing gate
         ["radio_defense"] = true, -- Wiretap                    // ends when Lange climbed the tower
         ["bunker_test"] = true,   -- Bunker Busters             // ends when blowing up the dynamite
         ["spies_test"] = true,    -- Extraction                 // ends when using the radio
         ["tutorial"] = true,      -- 'Tutorial Online' compat   // ends when warcry is shown to the player
-        ["fake_mission_clear_skies_radio_defense"] = true, -- 'Operation days as missions' compat (London calling / clear skies 4/6)
+
+        -- 'Operation days as missions' compat:
+        ["fake_mission_clear_skies_mini_raid_1_park"] = true,        -- Communication Breakdown / Clear Skies 1/6
+        ["fake_mission_clear_skies_mini_raid_2_park"] = true,        -- Burning Man / Clear Skies 3/6
+        ["fake_mission_clear_skies_radio_defense"] = true,           -- London Calling / Clear Skies 4/6
+        ["fake_mission_oper_flamable_mini_raid_1_park"] = true,      -- Cloak And Dagger / Rhinegold 1/4
+        ["fake_mission_oper_flamable_mini_raid_2_destroyed"] = true, -- Fuel For The Fire / Rhinegold 2/4
+    }
+
+    local override_not_stealthable = {
+        ["silo"] = true, -- Countdown               // doesnt even actually start in stealth
     }
 
     function RaidGUIControlListItemRaids:_layout_raid_name(params, data, ...)
@@ -19,9 +31,11 @@ if string.lower(RequiredScript) == "lib/managers/menu/raid_menu/controls/raidgui
         end
 
         local raid_data = tweak_data.operations.missions[data.value]
-        if raid_data.start_in_stealth then
+        if ((raid_data.start_in_stealth or raid_data.stealth_bonus) and not override_not_stealthable[data.value]) or override_partially_stealthable[data.value] then
             local x, _, w, _ = self._item_label:text_rect()
-            self._stealhtable_icon = self:_init_stealthable_icon(x + w + 4, 0, raid_data.stealth_bonus == nil or partially_stealthable[data.value])
+            local partially_by_game = not (raid_data.start_in_stealth and raid_data.stealth_bonus)
+            self._stealhtable_icon = self:_init_stealthable_icon(x + w + 4, 0,
+                partially_by_game or override_partially_stealthable[data.value])
             self._stealhtable_icon:set_center_y(RaidGUIControlListItemRaids.NAME_CENTER_Y)
         end
     end
