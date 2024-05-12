c2134c6efb3361d1981ae81df25c8ca69281e9de
BangL <henno.rickowski@gmail.com>
2024-05-12 18:02:17 +0200

    add ingame host options to pause menu

--- modules/GameSettings.lua
+++ modules/GameSettings.lua
@@ -36,12 +36,15 @@ if string.lower(RequiredScript) == "lib/managers/menu/raid_menu/missionselection
     function MissionSelectionGui:_on_permission_selected(data, ...)
         local result = _on_permission_selected_original(self, data, ...)
 
-        local old_value = Global.player_manager.permission
+        local old_value = Global.game_settings.permission
         if old_value ~= data.value then
             WolfgangHUD:setSetting({ "GAME_SETTINGS", "PERMISSION" }, table.index_of(tweak_data.permissions, data.value))
             WolfgangHUD:Save()
 
-            Global.player_manager.permission = data.value
+            Global.game_settings.permission = data.value
+
+            managers.network:session():chk_server_joinable_state()
+            managers.network:update_matchmake_attributes()
         end
 
         return result
@@ -56,6 +59,9 @@ if string.lower(RequiredScript) == "lib/managers/menu/raid_menu/missionselection
             WolfgangHUD:Save()
 
             Global.game_settings.drop_in_allowed = value
+
+            managers.network:session():chk_server_joinable_state()
+            managers.network:update_matchmake_attributes()
         end
 
         return result
@@ -198,11 +204,13 @@ elseif string.lower(RequiredScript) == "lib/managers/dynamicresourcemanager" the
 
     function DynamicResourceManager:init(...)
         if Network:is_server() and Global.game_settings.level_id == OperationsTweakData.ENTRY_POINT_LEVEL then
-            Global.game_settings.difficulty = tweak_data.difficulties
+            Global.DEFAULT_DIFFICULTY = tweak_data.difficulties
                 [WolfgangHUD:getSetting({ "GAME_SETTINGS", "DIFFICULTY" }, 2)]
+            Global.game_settings.difficulty = Global.DEFAULT_DIFFICULTY
             if not Global.game_settings.single_player then
-                Global.game_settings.permission = tweak_data.permissions
+                Global.DEFAULT_PERMISSION = tweak_data.permissions
                     [WolfgangHUD:getSetting({ "GAME_SETTINGS", "PERMISSION" }, 1)]
+                Global.game_settings.permission = Global.DEFAULT_PERMISSION
                 Global.game_settings.drop_in_allowed = WolfgangHUD:getSetting({ "GAME_SETTINGS", "DROP_IN_ALLOWED" },
                     true)
                 Global.game_settings.selected_team_ai = WolfgangHUD:getSetting({ "GAME_SETTINGS", "TEAM_AI" }, true)
@@ -211,5 +219,133 @@ elseif string.lower(RequiredScript) == "lib/managers/dynamicresourcemanager" the
             end
         end
         return init_original(self, ...)
+    end
+elseif string.lower(RequiredScript) == "lib/managers/menu/raid_menu/raidmainmenugui" then
+    local _layout_original = RaidMainMenuGui._layout
+
+    function RaidMainMenuGui:_layout(...)
+        _layout_original(self, ...)
+
+        if not (managers.raid_job and managers.raid_job:is_in_tutorial())
+            and (managers.platform:rich_presence() == "MPPlaying" or managers.platform:rich_presence() == "MPLobby")
+            and not Global.game_settings.single_player then
+            self:wg_layout_right_panel()
+            self:wg_layout_game_settings()
+        end
+    end
+
+    function RaidMainMenuGui:wg_layout_right_panel()
+        local right_panel_params = {
+            name = "right_panel",
+            h = 640,
+            y = 192,
+            w = 480,
+            x = 0,
+            layer = 1
+        }
+
+        self._right_panel = self._root_panel:panel(right_panel_params)
+        self._right_panel:set_x(self._root_panel:w() - self._right_panel:w())
+    end
+
+    function RaidMainMenuGui:wg_layout_game_settings()
+        self._settings_controls = {}
+
+        local settings_panel_params = {
+            name = "settings_panel"
+        }
+
+        self._settings_panel = self._right_panel:panel(settings_panel_params)
+
+        local permission_stepper_params = {
+            name = "permission_stepper",
+            x = 0,
+            y = 0,
+            description = self:translate("menu_permission_title", true),
+            on_item_selected_callback = callback(self, self, "wg_on_permission_selected"),
+            data_source_callback = callback(self, self, "wg_data_source_permission_stepper"),
+            on_menu_move = {
+                down = "drop_in_checkbox",
+                left = "audio_button"
+            }
+        }
+
+        self._permission_stepper = self._settings_panel:stepper(permission_stepper_params)
+
+        self._permission_stepper:set_value_and_render(Global.game_settings.permission, true)
+        table.insert(self._settings_controls, self._permission_stepper)
+
+        local drop_in_checkbox_params = {
+            name = "drop_in_checkbox",
+            value = true,
+            x = 0,
+            y = self._permission_stepper:y() + self._permission_stepper:h() + MissionSelectionGui.SETTINGS_PADDING,
+            description = self:translate("menu_allow_drop_in_title", true),
+            on_click_callback = callback(self, self, "wg_on_toggle_drop_in"),
+            on_menu_move = {
+                up = "permission_stepper",
+                down = "team_ai_checkbox",
+                left = "audio_button"
+            }
+        }
+
+        self._drop_in_checkbox = self._settings_panel:toggle_button(drop_in_checkbox_params)
+
+        self._drop_in_checkbox:set_value_and_render(Global.game_settings.drop_in_allowed)
+        table.insert(self._settings_controls, self._drop_in_checkbox)
+    end
+
+    function RaidMainMenuGui:wg_data_source_permission_stepper()
+        local permissions = {}
+
+        table.insert(permissions, {
+            value = "public",
+            info = "public",
+            text = self:translate("menu_permission_public", true)
+        })
+        table.insert(permissions, {
+            value = "friends_only",
+            info = "friends_only",
+            text = self:translate("menu_permission_friends", true)
+        })
+        table.insert(permissions, {
+            value = "private",
+            info = "private",
+            text = self:translate("menu_permission_private", true)
+        })
+
+        return permissions
+    end
+
+    function RaidMainMenuGui:wg_on_permission_selected(data)
+        local old_value = Global.game_settings.permission
+        if old_value ~= data.value then
+            WolfgangHUD:setSetting({ "GAME_SETTINGS", "PERMISSION" }, table.index_of(tweak_data.permissions, data.value))
+            WolfgangHUD:Save()
+
+            Global.game_settings.permission = data.value
+            if Global.player_manager then
+                Global.player_manager.game_settings_permission = data.value
+            end
+
+            managers.network:session():chk_server_joinable_state()
+            managers.network:update_matchmake_attributes()
+        end
+    end
+
+    function RaidMainMenuGui:wg_on_toggle_drop_in(_, _, value)
+        local old_value = Global.game_settings.drop_in_allowed
+        if old_value ~= value then
+            WolfgangHUD:setSetting({ "GAME_SETTINGS", "DROP_IN_ALLOWED" }, value)
+            WolfgangHUD:Save()
+
+            Global.game_settings.drop_in_allowed = value
+            if Global.player_manager then
+                Global.player_manager.game_settings_drop_in_allowed = value
+            end
+
+            managers.network:session():chk_server_joinable_state()
+            managers.network:update_matchmake_attributes()
+        end
     end
 end
